1478f2fa937f768ff9199019cf73940a64b2630b
曾義格 <tsengeagle@gmail.com>
2016-12-24 21:25:44 +0800

    當系統內有相依元件時，測試就比較複雜了

---
+++ Lab/Lab/StubAndMock.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace Lab
+{
+    public class StubAndMock
+    {
+        internal void DoSomething()
+        {
+            var logger = new MyLog();
+            logger.WriteMessage("執行到此");
+        }
+    }
+
+    [Microsoft.VisualStudio.TestTools.UnitTesting.TestClass]
+    public class StubAndMockTests
+    {
+        [TestMethod]
+        public void SUT_Behavior_UseMockToSimulationDependencies()
+        {
+            //arrange
+            var target=new StubAndMock();
+
+            //act   
+            target.DoSomething();
+
+            //assert??? <----如何確認DoSomething這個method真的有寫log?
+            
+        }
+    }
+
+    public class MyLog
+    {
+        public void WriteMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
