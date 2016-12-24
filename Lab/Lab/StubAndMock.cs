82ec0d6e68577da08caff9e036cad88f96879c6a
曾義格 <tsengeagle@gmail.com>
2016-12-24 21:52:36 +0800

    IoC相依元件之後，用mock模擬相依元件注入 依賴的方法必須virtual

--- Lab/Lab/StubAndMock.cs
+++ Lab/Lab/StubAndMock.cs
@@ -1,36 +1,66 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
 namespace Lab
 {
     public class StubAndMock
     {
+        private MyLog _logger;
+
+        public StubAndMock()
+        {
+            _logger = new MyLog();
+        }
+
+        public StubAndMock(MyLog logger)
+        {
+            _logger = logger;
+        }
+
         internal void DoSomething()
         {
-            var logger = new MyLog();
-            logger.WriteMessage("執行到此");
+            _logger.WriteMessage("執行到此");
         }
     }
 
     [Microsoft.VisualStudio.TestTools.UnitTesting.TestClass]
     public class StubAndMockTests
     {
+        [Ignore]
         [TestMethod]
-        public void SUT_Behavior_UseMockToSimulationDependencies()
+        public void SUT_Behavior_AssertInteractWithDependencies()
         {
             //arrange
-            var target=new StubAndMock();
+            var target = new StubAndMock();
 
             //act   
             target.DoSomething();
 
             //assert??? <----如何確認DoSomething這個method真的有寫log?
-            
+
+        }
+
+        [TestMethod]
+        public void SUT_Behavior_UseMockToSimulateDependencies()
+        {
+            //arrange
+            var mockObject = new Mock<MyLog>();
+            //mockObject.Setup(s => s.WriteMessage(It.IsAny<string>()));
+            var logger = mockObject.Object;
+            var target = new StubAndMock(logger);
+
+            //act
+            target.DoSomething();
+
+            //assert
+            mockObject.Verify(v => v.WriteMessage(It.IsAny<string>()), Times.Exactly(1));
         }
     }
 
     public class MyLog
     {
-        public void WriteMessage(string message)
+        public virtual void WriteMessage(string message)
         {
             Console.WriteLine(message);
         }
