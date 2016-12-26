5866d2cb1e30f5654283bc6ebc6f2ed7ddc9de08
曾義格 <tsengeagle@gmail.com>
2016-12-26 20:35:54 +0800

    練習

---
+++ Lab/ShiftScheduler/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShiftScheduler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    //Part1
+    //1.排班以月為單位
+    //2.每7天要有2天休假
+    //班別分為上班、休假
+    //驗證結果分為通過、失敗
+
+    //Part2
+    //1.排班以月為單位
+    //2.每2周要有2天休假
+    //3.每4周休假+休息至少8天
+    //班別分為上班、休息、休假
+    //驗證結果為通過(true)或失敗(false)
+}
