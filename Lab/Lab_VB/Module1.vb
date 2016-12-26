85a3752ee0be47817aa1e84a883a76393332c0aa
曾義格 <tsengeagle@gmail.com>
2016-12-26 20:55:03 +0800

    vb.net的單元測試

---
+++ Lab/Lab_VB/Module1.vb
@@ -0,0 +1,19 @@
+﻿Imports Microsoft.VisualStudio.TestTools.UnitTesting
+Module Module1
+
+    Sub Main()
+
+    End Sub
+
+End Module
+
+<TestClass()>
+Public Class MyTest
+
+    <TestMethod()>
+    Public Sub MyTestMethod()
+
+        Assert.AreEqual(1, 1)
+    End Sub
+
+End Class
