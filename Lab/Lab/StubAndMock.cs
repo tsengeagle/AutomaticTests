da89b98db664f64718d7869ae35014d4d5af8db1
曾義格 <tsengeagle@gmail.com>
2016-12-27 16:30:29 +0800

    還原

--- Lab/Lab/StubAndMock.cs
+++ Lab/Lab/StubAndMock.cs
@@ -1,59 +1,36 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Moq;
-
 namespace Lab
 {
     public class StubAndMock
     {
-        private MyLog _logger;
-
-        public StubAndMock()
-        {
-            _logger = new MyLog();
-        }
-
-        //public StubAndMock(MyLog logger)
-        //{
-        //    _logger = logger;
-        //}
-
         internal void DoSomething()
         {
-            _logger.WriteMessage("執行到此");
+            var logger = new MyLog();
+            logger.WriteMessage("執行到此");
         }
     }
 
     [Microsoft.VisualStudio.TestTools.UnitTesting.TestClass]
     public class StubAndMockTests
     {
-        [Ignore]
         [TestMethod]
-        public void SUT_Behavior_AssertInteractWithDependencies()
+        public void SUT_Behavior_UseMockToSimulationDependencies()
         {
             //arrange
+            var target=new StubAndMock();
 
             //act   
+            target.DoSomething();
 
             //assert??? <----如何確認DoSomething這個method真的有寫log?
-
-        }
-
-        [TestMethod]
-        public void SUT_Behavior_UseMockToSimulateDependencies()
-        {
-            //arrange
-
-            //act
-
-            //assert
-
+            
         }
     }
 
     public class MyLog
     {
-        public virtual void WriteMessage(string message)
+        public void WriteMessage(string message)
         {
             Console.WriteLine(message);
         }
